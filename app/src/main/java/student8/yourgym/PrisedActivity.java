902a72d47a745fd35285f3926afa0715bd3d6773
Damir <damir180501>
2017-12-18 23:32:47 +0300

    ar

--- app/src/main/java/student8/yourgym/PrisedActivity.java
+++ app/src/main/java/student8/yourgym/PrisedActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 
 public class PrisedActivity extends Activity {
 
-    TextView tvName, tvInfo, tv[][][] = new TextView[12][8][3];
+    TextView tvInfo, tv11, tv12, tv13, tv14, tv15, tv21, tv22, tv23, tv24, tv25, tv26, tv31, tv32, tv33, tv34, tv35, tv36, tv37;
     public static final String MK = "myGym";
     public static final String MP = "prised";
     private SharedPreferences mSet;
@@ -21,19 +21,67 @@ public class PrisedActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prised);
         names();
-        tvInfo.setText(tvInfo.getText() + (mSet.getFloat(MP, 0) + " кг жмет"));
-        tv[0][0][0].setText("0 0 0");
-        tv[0][0][1].setText("0 0 1");
+        values();
+        tvInfo.setText(tvInfo.getText() + (mSet.getFloat(MP, 0) + ""));
     }
 
     void names(){
-        tvName = findViewById(R.id.textViewName);
         tvInfo = findViewById(R.id.textViewInfo);
         mSet = getSharedPreferences(MK, Context.MODE_PRIVATE);
 
-        for(int k = 0; k < 12; k++)
-            for(int i = 0; i < 8; i++)
-                for(int j = 0; j < 3; j++)
-                    tv[k][i][j] = findViewById(R.id.textView1_1_1+i*j*k);
+        tv11 = findViewById(R.id.textView1_2_1);
+        tv12 = findViewById(R.id.textView1_3_1);
+        tv13 = findViewById(R.id.textView1_4_1);
+        tv14 = findViewById(R.id.textView1_5_1);
+        tv15 = findViewById(R.id.textView1_6_1);
+
+        tv21 = findViewById(R.id.textView2_2_1);
+        tv22 = findViewById(R.id.textView2_3_1);
+        tv23 = findViewById(R.id.textView2_4_1);
+        tv24 = findViewById(R.id.textView2_5_1);
+        tv25 = findViewById(R.id.textView2_6_1);
+        tv26 = findViewById(R.id.textView2_7_1);
+
+        tv31 = findViewById(R.id.textView3_2_1);
+        tv32 = findViewById(R.id.textView3_3_1);
+        tv33 = findViewById(R.id.textView3_4_1);
+        tv34 = findViewById(R.id.textView3_5_1);
+        tv35 = findViewById(R.id.textView3_6_1);
+        tv36 = findViewById(R.id.textView3_7_1);
+        tv37 = findViewById(R.id.textView3_8_1);
+    }
+
+    double cycle(double ok){
+        int oki = (int)(ok*100);
+        oki %= 100;
+        if(oki < 13)return (int)ok;
+        if(oki < 38 && oki >= 13)return (int)ok + 0.25;
+        if(oki < 63 && oki >= 38)return (int)ok + 0.5;
+        if(oki < 88 && oki >= 63)return (int)ok + 0.75;
+        if(oki >= 88)return (int)ok + 1;
+        return ok;
+    }
+
+    void values(){
+        tv11.setText(cycle(mSet.getFloat(MP, 0)*0.3) + "");
+        tv12.setText(cycle(mSet.getFloat(MP, 0)*0.42) + "");
+        tv13.setText(cycle(mSet.getFloat(MP, 0)*0.56) + "");
+        tv14.setText(cycle(mSet.getFloat(MP, 0)*0.6) + "");
+        tv15.setText(cycle(mSet.getFloat(MP, 0)*0.64) + "");
+
+        tv21.setText(cycle(mSet.getFloat(MP, 0)*0.3) + "");
+        tv22.setText(cycle(mSet.getFloat(MP, 0)*0.42) + "");
+        tv23.setText(cycle(mSet.getFloat(MP, 0)*0.56) + "");
+        tv24.setText(cycle(mSet.getFloat(MP, 0)*0.64) + "");
+        tv25.setText(cycle(mSet.getFloat(MP, 0)*0.72) + "");
+        tv26.setText(cycle(mSet.getFloat(MP, 0)*0.76) + "");
+
+        tv31.setText(cycle(mSet.getFloat(MP, 0)*0.3) + "");
+        tv32.setText(cycle(mSet.getFloat(MP, 0)*0.54) + "");
+        tv33.setText(cycle(mSet.getFloat(MP, 0)*0.68) + "");
+        tv34.setText(cycle(mSet.getFloat(MP, 0)*0.76) + "");
+        tv35.setText(cycle(mSet.getFloat(MP, 0)*0.84) + "");
+        tv36.setText(cycle(mSet.getFloat(MP, 0)*0.92) + "");
+        tv37.setText(cycle(mSet.getFloat(MP, 0)*0.72) + "");
     }
 }
